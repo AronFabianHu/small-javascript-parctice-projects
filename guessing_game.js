7511e489ce328d3adbfd269a0e39549482a347d8
Aron Fabian <130830737+AronFabianHu@users.noreply.github.com>
2023-05-26 22:39:29 +0200

    Add files via upload

---
+++ guessing_game.js
@@ -0,0 +1,32 @@
+let maximum = parseInt(prompt("Enter the maximum number"))
+while (!maximum) {
+    maximum = parseInt(prompt("Enter a valid number"))
+}
+
+const targetNum = Math.floor(Math.random() * maximum) + 1;
+
+let guess = (prompt("Enter your first guess! (Type 'q' if you want to quit!)"));
+let attempts = 1;
+
+while (parseInt(guess) !== targetNum) {
+    if (guess === "q") {
+        break;
+    }
+
+    guess = parseInt(guess)
+
+    if (guess > targetNum) {
+        guess = (prompt("Your number is too high! Enter a new guess:"))
+        attempts++;
+    } else if (guess < targetNum) {
+        guess = (prompt("Your number is too low! Enter a new guess:"))
+        attempts++;
+    } else {
+        guess = (prompt("Invalid guess. Please enter a number or 'q' to quit"))
+    }
+}
+if (guess === "q") {
+    console.log("Ok, you quit!")
+} else {
+    console.log(`You got it! It took you ${attempts} guess(es).`)
+}
