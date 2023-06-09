25b191f2719e1bd9bbd1de686d658fe168f43487
Aron Fabian <130830737+AronFabianHu@users.noreply.github.com>
2023-06-09 21:52:47 +0200

    Add files via upload

--- random-background-color/app.js
+++ random-background-color/app.js
@@ -1,20 +1,32 @@
 
 
-const r = function () { return Math.floor(Math.random() * 256) };
-const g = function () { return Math.floor(Math.random() * 256) };
-const b = function () { return Math.floor(Math.random() * 256) };
+// const r = function () { return Math.floor(Math.random() * 256) };
+// const g = function () { return Math.floor(Math.random() * 256) };
+// const b = function () { return Math.floor(Math.random() * 256) };
+
+// const h1 = document.querySelector('#rgb')
+// const button = document.querySelector('#colorbtn')
+// const body = document.querySelector('body')
+
+
+
+// button.addEventListener('click', function () {
+//     body.style.backgroundColor = `rgb(${r()},${g()},${b()})`
+// })
+
+// button.addEventListener('click', function () {
+//     h1.innerHTML = `rgb(${r()}, ${g()}, ${b()})`
+// })
 
 const h1 = document.querySelector('#rgb')
 const button = document.querySelector('#colorbtn')
 const body = document.querySelector('body')
 
 
-
 button.addEventListener('click', function () {
-    body.style.backgroundColor = `rgb(${r()},${g()},${b()})`
+    const r = Math.floor(Math.random() * 256)
+    const g = Math.floor(Math.random() * 256)
+    const b = Math.floor(Math.random() * 256)
+    h1.innerHTML = `Your random background color is: rgb(${r}, ${g}, ${b})`
+    body.style.backgroundColor = `rgb(${r}, ${g}, ${b})`
 })
-
-button.addEventListener('click', function () {
-    h1.innerHTML = `rgb(${r()}, ${g()}, ${b()})`
-})
-
